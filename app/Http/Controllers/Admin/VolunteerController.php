a91f73032d4dc817bf84123fc49f22ada118caa3
Hengki2003 <hengkiagng@gmail.com>
2025-03-26 09:02:05 +0800

    add

--- app/Http/Controllers/Admin/VolunteerController.php
+++ app/Http/Controllers/Admin/VolunteerController.php
@@ -119,9 +119,12 @@ class VolunteerController extends Controller
         }
 
         $all = $query->count();
-        $other = $query->where('status', Constant::VOLUNTEERS_STATUS["Hanya taruh brosur"])->count();
-        $interest = $query->where('status', Constant::VOLUNTEERS_STATUS['Tertarik dengan produk'])->count();
-        $notInterest = $query->where('status', Constant::VOLUNTEERS_STATUS["Tidak tertarik"])->count();
+        $other = $query;
+        $other = $other->where('status', Constant::VOLUNTEERS_STATUS["Hanya taruh brosur"])->count();
+        $interest = $query;
+        $interest = $interest->where('status', Constant::VOLUNTEERS_STATUS['Tertarik dengan produk'])->count();
+        $notInterest = $query;
+        $notInterest = $notInterest->where('status', Constant::VOLUNTEERS_STATUS["Tidak tertarik"])->count();
 
         return response()->json([
             'all' => $all,
